b39b5bdf4a009da2d3fa33e842ca922215a3ca2a
Noe (Titan) <vnmendoza94@gmail.com>
2020-06-11 16:32:43 -0700

    first commit

---
+++ HttpClientTester.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+import java.nio.charset.*;
+import java.util.*;
+
+//import sun.net.www.http.HttpClient;
+public class HttpClientTester
+// @author Victor Mendoza
+{
+    private final static String URL = "https://cisp21profh.pythonanywhere.com/api/spring2020";
+    public static void main(final String[] args) throws IOException
+        {        
+            HttpClient client = HttpClient.newHttpClient();
+           // HttpRequest
+        }
+    
+}
