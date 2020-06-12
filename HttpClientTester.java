1ea5eaa9f227decf1cc10cd130c01a344acbe1a0
vnmendoza_Libro <vnmendoza94@gmail.com>
2020-06-11 19:57:26 -0700

    finished product

--- HttpClientTester.java
+++ HttpClientTester.java
@@ -1,22 +1,19 @@
 import java.io.*;
 import java.net.*;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.*;
 import java.nio.charset.*;
 import java.util.*;
-import org.apache.http.*;
-import sun.net.www.http.HttpClient;
-//import org.apache.http.client.HttpClient;
-//import org.apache.http.client.methods.HttpPost;
-//import org.apache.http.impl.client.DefaultHttpClient;
 public class HttpClientTester
 // @author Victor Mendoza
 {
     private final static String URL = "https://cisp21profh.pythonanywhere.com/api/spring2020";
-    public static void main(final String[] args) throws IOException
+    public static void main(final String[] args) throws IOException, URISyntaxException, InterruptedException
         {        
-            //java.net.http.HttpClient client = HttpClient.newHttpClient();
-           HttpClient client = HttpClient.newHttpClient();
+           java.net.http.HttpClient client = HttpClient.newHttpClient();
+           //HttpClient client = HttpClient.newHttpClient();
 
             if (client != null)
             {
@@ -28,9 +25,24 @@ public class HttpClientTester
                     HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
                     if(response != null)
                     {
-
+                        String bodyString = response.body();
+                        HttpHeaders responseHeaders = response.headers();
+                        Map<String, List<String>> headerMap = responseHeaders.map();
+                        //String responseHeaders = response.headers();
+                        System.out.println("response.body:");
+                        System.out.println(bodyString);
+                        System.out.println("response.headers:");
+                        
+                        for (Map.Entry<String, List<String>> entry : headerMap.entrySet())
+                        {
+                            if(entry.getKey().toString().matches("date|content-type|content-length"))
+                            {
+                                System.out.println(entry.getKey().toString() + ": " + entry.getValue().toString());    
+                            }
+                        }
                     }
                 }          
             } 
         }
     
+}
